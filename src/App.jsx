8af0b3dec8308f500dd0a532080c89cf58667a62
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-07 19:31:30 +0200

    Added page not found

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,12 @@ import HostVanDetail from "./pages/Host/HostVanDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
 import "./server"
+
 
 function App() {
   return (
@@ -39,6 +41,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />}/>
         </Route>
       </Routes>
     </BrowserRouter>
